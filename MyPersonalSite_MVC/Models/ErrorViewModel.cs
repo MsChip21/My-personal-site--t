f9b4f147cbea679641678fd763657ed933a00412
Taha Dashti <mohammadtahadashti1221@gmail.com>
2023-01-04 12:52:08 +0330

    repository initialized

---
+++ MyPersonalSite_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyPersonalSite_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
